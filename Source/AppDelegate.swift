0564529a89d0a932021beccaf7df8b13ad75c6a0
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-04-07 09:55:03 -0400

    Organize project

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  LoadFile
+//
+//  Created by Jeffrey Kereakoglow on 4/7/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
---
+++ Source/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  LoadFile
+//
+//  Created by Jeffrey Kereakoglow on 4/7/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
